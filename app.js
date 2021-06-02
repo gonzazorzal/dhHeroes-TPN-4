c17c2c9b8d87341bacd3b3594c2d058970f5d259
gonzazorzal <gonzalo220201@gmail.com>
2021-06-02 06:46:45 -0300

    TPDHheroes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express= require ('express');
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3030;
 const path = require('path');
-console.log(__dirname);
+const views = path.join(__dirname, 'views');
+app.use(express.static('public'));
 
-app.listen(port, ()=> console.log('Servidor corriendo en el puerto ' + port));
-app.get('/', (req,res) => res.send('HOME'))
+app.get('/home', (req, res) => res.sendFile(path.join(views,'index.html')));
+app.get('/babbage', (req, res) => res.sendFile(path.join(views,'babbage.html')));
+app.get('/berners-lee', (req, res) => res.sendFile(path.join(views,'berners-lee.html')));
+app.get('/clarke', (req, res) => res.sendFile(path.join(views,'clarke.html')));
+app.get('/hamilton', (req, res) => res.sendFile(path.join(views,'hamilton.html')));
+app.get('/hopper', (req, res) => res.sendFile(path.join(views,'hopper.html')));
+app.get('/lovelace', (req, res) => res.sendFile(path.join(views,'lovelace.html')));
+app.get('/turing', (req, res) => res.sendFile(path.join(views,'turing.html')));
+
+
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
+
 
